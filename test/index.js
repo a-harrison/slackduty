1b78b99acf9992418ad24f9e73702f401988efc8
Adam Harrison <adamaharrison@gmail.com>
2018-02-08 12:34:26 -0800

    Removed extraneous test api key declaration.

--- test/index.js
+++ test/index.js
@@ -8,8 +8,6 @@ var testtube = carbon.testtube
 /***************************************************************************************************
  * TEST_API_KLEY
  */
-var TEST_API_KEY = "abcdefghi1234567890"
-
 __(function() {
   module.exports = o.main({
     _type: carbon.carbond.test.ServiceTest,
